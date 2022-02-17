3be7a8747336aba045ae2fd5af59062a33214fb9
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-02-17 21:01:51 +0600

    saving part added

--- js/calculation.js
+++ js/calculation.js
@@ -11,13 +11,14 @@ function moneyAmount(idname) {
     const insertAmount = document.getElementById(idname);
     const totalAmount = insertAmount.value;
     const money = parseFloat(totalAmount);
-    if(money>0){
-       return money; 
-    }else{
+    if (money > 0) {
+        return money;
+    } else {
         return errorMessage();
     }
 }
 
+var leftBalance;
 
 //Calculation Part
 document.getElementById('calculate-btn').addEventListener('click', function () {
@@ -34,10 +35,23 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
         //balance
         const balanceAmount = document.getElementById('balance-amount');
-        balanceAmount.innerText = incomeMoney - totalExpences;
-    } else if (totalExpences > incomeMoney) {
+        leftBalance = incomeMoney - totalExpences;
+        balanceAmount.innerText = leftBalance;
+    } else {
         errorMessage();
     }
+})
 
+//saving part
+document.getElementById('save-btn').addEventListener('click', function () {
+    const savingMoneyparcent = moneyAmount('save-amount');
+    const savingAmount = leftBalance * (savingMoneyparcent / 100);
 
+    const saveMoney = document.getElementById('save-money');
+    saveMoney.innerText=savingAmount;
+
+    // console.log(savingAmount);
+    // console.log(leftBalance);
+    const remainingBalance = document.getElementById('remaining-value');
+    remainingBalance.innerText= leftBalance - savingAmount;
 })
