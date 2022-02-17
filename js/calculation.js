bcd9a4354be84a25cb044a3a948da97845c2fd9b
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-02-17 20:26:56 +0600

    error messege  add

--- js/calculation.js
+++ js/calculation.js
@@ -1,23 +1,43 @@
+//error msg
+function errorMessage() {
+    const error = document.getElementById('error');
+    // Changing content and color of content
+    error.textContent = "Please enter a valid number";
+    error.style.color = "red";
+}
+
 //money Amount
-function moneyAmount(idname){
+function moneyAmount(idname) {
     const insertAmount = document.getElementById(idname);
     const totalAmount = insertAmount.value;
-    const money=parseFloat(totalAmount);
-    // console.log(money);
+    const money = parseFloat(totalAmount);
+    if(money>0){
+       return money; 
+    }else{
+        return errorMessage();
+    }
 }
 
 
 //Calculation Part
 document.getElementById('calculate-btn').addEventListener('click', function () {
     //Income Part
-    moneyAmount('income-amount');
+    const incomeMoney = moneyAmount('income-amount');
 
-    //food
-    moneyAmount('food-amount');
+    //total expences
+    const totalExpences = moneyAmount('food-amount') + moneyAmount('rent-amount') + moneyAmount('clothes-amount');
 
-    //Rent
-    moneyAmount('rent-amount');
+    if (totalExpences < incomeMoney && totalExpences > 0 && incomeMoney > 0) {
+        //expence
+        const expencesAmount = document.getElementById('total-expences');
+        expencesAmount.innerText = totalExpences;
 
-    //clothes
-    moneyAmount('clothes-amount');
+        //balance
+        const balanceAmount = document.getElementById('balance-amount');
+        balanceAmount.innerText = incomeMoney - totalExpences;
+    } else if (totalExpences > incomeMoney) {
+        errorMessage();
+    }
+
+
 })
